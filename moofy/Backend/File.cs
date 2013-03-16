f131cf8adb0547192a68770a9a112f967f4bd647
Niclas Tollstorff <niclastollstorff@gmail.com>
2013-03-16 18:39:03 +0100

    Naive implementation of a File class

---
+++ moofy/Backend/File.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace moofy.Backend {
+    public interface File {
+        int Id {
+            get;
+        }
+
+        string Title {
+            get;
+            set;
+        }
+
+        string Description {
+            get;
+            set;
+        }
+
+        IList<string> Genre {
+            get;
+            set;
+        }
+
+        short Year {
+            get;
+            set;
+        }
+
+        string URI {
+            get;
+            set;
+        }
+
+        float RentPrice {
+            get;
+            set;
+        }
+
+        float BuyPrice {
+            get;
+            set;
+        }
+    }
+}
