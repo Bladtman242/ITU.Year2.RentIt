f523ef1f5d808a1a4ada1766367c72336babc631
Bladtman <sigurt91@gmail.com>
2013-03-16 23:35:08 +0100

    An (incomplete) attempt to host the json services in the testing suite, and call them over http 	modified:   moofy/JsonServicesTests/JsonServicesTests.csproj 	modified:   moofy/JsonServicesTests/UnitTest1.cs

--- moofy/JsonServicesTests/UnitTest1.cs
+++ moofy/JsonServicesTests/UnitTest1.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using moofy.JsonServices;
+using System;
+using System.Net;
+using System.Threading;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace moofy.JsonServices.Tests {
+    
     [TestClass]
     public class UnitTest1 {
+
+        ServiceHost host = null;
+
+        //This should probably be a test-utility class on its own
+        [AssemblyInitialize]
+        public static void InitiateServices(TestContext t) {
+            Uri baseAddr = new Uri("http://localhost:8732/moofytest");
+
+            using (ServiceHost host = new ServiceHost(typeof(MoofyServices), baseAddr)) {
+                host.Open();
+            }
+        }
+
         [TestMethod]
         public void TestMethod1() {
+            
         }
     }
 }
