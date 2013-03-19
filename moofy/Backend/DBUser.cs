7a1ea2f37530d21452af9a57cd82fd406f4b9adf
Gansted <kbri@itu.dk>
2013-03-19 20:26:04 +0100

    Added functionality to DBUser

--- moofy/Backend/DBUser.cs
+++ moofy/Backend/DBUser.cs
@@ -12,24 +12,134 @@ namespace moofy.Backend {
                                        "database=RENTIT25; " +
                                        "connection timeout=30");
 
-        //public static int AddUser(User user) {
-        //    try {
-        //        connection.Open();
-        //        SqlCommand command = new SqlCommand("INSERT INTO Userz VALUES ('" + 
-        //                                                user.Username + "', '" + 
-        //                                                user.Password + "', '" + 
-        //                                                user.Name + "', '" + 
-        //                                                user.Email + "', " + 
-        //                                                user.Balance + ")",
-        //                                                connection);
-        //        SqlDataReader reader = command.ExecuteReader();
-        //    } catch (Exception e) {
-        //        Console.WriteLine(e.ToString());
-        //    } finally {
-        //        connection.Close();
-        //    }
-        //}
+        /// <summary>
+        /// Promoter a user to admin status
+        /// </summary>
+        /// <param name="promoterId">The ID of an admin who accepts this user as an admin</param>
+        /// <param name="promoteeId">The user to be promoted</param>
+        /// <returns>succes flag</returns>
+        public static bool PromotetoAdmin(int promoterId, int promoteeId)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Admin WHERE id =" + promoterId, connection);
+                if (command.ExecuteScalar() != null)
+                {
+                    command.CommandText = "INSERT INTO Admin VALUES(" + promoteeId + ")";
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+ 
+        /// <summary>
+        /// Deposits and amount to a user
+        /// </summary>
+        /// <param name="amount">The amount to deposit</param>
+        /// <param name="userId">The Id of the user who will have the amount deposited</param>
+        /// <returns>success flag</returns>
+        public static bool Deposit(int amount, int userId)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE Userz" +
+                                                    "SET balance=balance+" + amount +
+                                                    "WHERE id=" + userId,
+                                                    connection);
+                return command.ExecuteNonQuery() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
 
+        /// <summary>
+        /// Checks if a user with a given username and password is valid
+        /// </summary>
+        /// <param name="uname">The username to check</param>
+        /// <param name="password">The password to check</param>
+        /// <returns>success flag</returns>
+        public static int Login(string uname, string password)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT id FROM Userz" +
+                                                    "WHERE password=" + password +
+                                                    "AND userName=" + uname,
+                                                    connection);
+                Object id = command.ExecuteScalar();
+                if (id != null) return (int)id;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Add a new user to the database
+        /// </summary>
+        /// <param name="user">The user to add</param>
+        /// <returns>the id of the user, or -1 if the user could not be added</returns>
+        public static int AddUser(User user)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Userz(userName, password, name, email, balance)" +
+                                                        "VALUES ('" +
+                                                        user.Username + "', '" +
+                                                        user.Password + "', '" +
+                                                        user.Name + "', '" +
+                                                        user.Email + "', " +
+                                                        user.Balance + ")",
+                                                        connection);
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    command.CommandText = "SELECT IDENT_CURRENT('Userz')";
+                    return (int)command.ExecuteScalar();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns a user from the database
+        /// </summary>
+        /// <param name="userId">The id of the user to return</param>
+        /// <returns>The user with the given id or null if no such user exists</returns>
         public static User GetUser(int userId) {
             try {
                 connection.Open();
@@ -56,6 +166,11 @@ namespace moofy.Backend {
             return null;
         }
 
+        /// <summary>
+        /// Returns a flag indicating wether a user is admin
+        /// </summary>
+        /// <param name="userId">The id of the user to check if is admin</param>
+        /// <returns>true if the user is admin else false</returns>
         public static bool getIsAdmin(int userId) {
             try {
                 connection.Open();
@@ -71,7 +186,11 @@ namespace moofy.Backend {
             }
             return false;
         }
-
+        /// <summary>
+        /// Returns all files purchased by a given user 
+        /// </summary>
+        /// <param name="userId">The id of the user to return all purchased files for</param>
+        /// <returns>All files purchased by the user</returns>
         public static IList<Purchase> getPurchases(int userId) {
             //Get information on which movies the user has purchased
             List<Purchase> purchases = new List<Purchase>();
