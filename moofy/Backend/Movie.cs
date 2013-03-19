41815f4fe6091ea8f85d6623b097aa6bea49187a
Niclas Tollstorff <niclastollstorff@gmail.com>
2013-03-19 17:43:39 +0100

    Added equality check to movie for use in testing

--- moofy/Backend/Movie.cs
+++ moofy/Backend/Movie.cs
@@ -5,56 +5,29 @@ using System.Text;
 
 namespace moofy.Backend {
     public class Movie : File {
-        //A set of backing fields - must explictly declare accessors Interface properties
-        private int id;
-        private short year;
-        private string title, description, uri;
-        private float buyPrice, rentPrice;
-        private IList<string> genres;
-
-        public Movie(int id) {
-            this.id = id;
-        }
-
-        public int Id {
-            get { return id; }
-        }
-
-        public string Title {
-            get { return title; }
-            set { title = value; }
-        }
-
-        public string Description {
-            get { return description; }
-            set { description = value; }
-        }
+        public Movie(int id) : base(id) {} //Call base constructor
 
         public string Director { get; set; }
 
-        public IList<string> Genres {
-            get { return genres; }
-            set { genres = value; }
+        //Equals override according to http://msdn.microsoft.com/en-us/library/ms173147(v=vs.80).aspx
+        public override bool Equals(System.Object obj) {
+            // If parameter cannot be cast to Movie return false.
+            Movie m = obj as Movie;
+            if ((System.Object)m == null) {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return base.Equals(obj) && (Director == m.Director);
         }
 
-        public short Year {
-            get { return year; }
-            set { year = value; }
+        public bool Equals(Movie m) {
+            // Return true if the fields match:
+            return base.Equals((File)m) && (Director == m.Director);
         }
 
-        public string Uri {
-            get { return uri; }
-            set { uri = value; }
-        }
-
-        public float BuyPrice {
-            get { return buyPrice; }
-            set { buyPrice = value; }
-        }
-
-        public float RentPrice {
-            get { return rentPrice; }
-            set { rentPrice = value; }
+        public override int GetHashCode() {
+            return Id;
         }
     }
 }
