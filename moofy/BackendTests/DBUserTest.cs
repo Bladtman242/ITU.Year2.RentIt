b8f405c1066a72a2ff8928f183bc2a995e54e178
Gansted <kbri@itu.dk>
2013-03-20 18:00:13 +0100

    Finished inital implementation of DBUser and DBUserTest

--- moofy/BackendTests/DBUserTest.cs
+++ moofy/BackendTests/DBUserTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace moofy.Backend.Tests {
@@ -15,6 +15,10 @@ namespace moofy.Backend.Tests {
             // TODO: Add constructor logic here
             //
         }
+        /// <summary>
+        /// Used for cleaning up during and after testing. Deletes a user with a given ID.
+        /// </summary>
+        
 
         [TestMethod]
         public void GetUserTest() {
@@ -48,6 +52,127 @@ namespace moofy.Backend.Tests {
             //No user
             User nonExistingUser = DBUser.GetUser(-2);
             Assert.AreEqual<User>(null, nonExistingUser);
+        }
+
+        [TestMethod]
+        public void AddUserTest()
+        {
+            //User to add. Note Id is not given here but generated and returned in the AddUser method
+            User expectedUser = new User(0)
+            {
+                Name = "AddTestUser",
+                Username = "AddTestUser",
+                Balance = 10,
+                Email = "testuser@add.com",
+                Password = "test"
+            };
+
+            int givenId = DBUser.AddUser(expectedUser);
+            Console.WriteLine(givenId);
+            User actualUser = DBUser.GetUser(givenId);
+
+            //Check that the user added to the database is the same as returned by the get method for the givenId
+            Assert.AreEqual<int>(givenId, actualUser.Id);
+            Assert.AreEqual<string>(expectedUser.Name, actualUser.Name);
+            Assert.AreEqual<string>(expectedUser.Username, actualUser.Username);
+            Assert.AreEqual<int>(expectedUser.Balance, actualUser.Balance);
+            Assert.AreEqual<string>(expectedUser.Email, actualUser.Email);
+            Assert.AreEqual<string>(expectedUser.Password, actualUser.Password);
+
+            //Cleanup
+            DBUser.deleteUser(givenId);
+        }
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            //Adding a user with easy testable login information.
+            User expectedUser = new User(0)
+            {
+                Name = "LoginTestUser",
+                Username = "Login",
+                Balance = 100,
+                Email = "testuser@login.com",
+                Password = "test"
+            };
+
+            int id = DBUser.AddUser(expectedUser);
+
+            //Test that the right ID of the user is returned
+            Assert.AreEqual(DBUser.Login("Login","test"), id);
+            //Test that -1 is returned for a user which does not exist
+            Assert.AreEqual(DBUser.Login("XXXXXXXXOOOOOOllIIIlllIIaaaaAAAAaadd", "kkieklaklmcmmenns"), -1);
+
+            //Cleanup
+            DBUser.deleteUser(id);
+        }
+
+        [TestMethod]
+        public void DepositTest()
+        {
+            //Adding a user with a known balance which is easy to test against.
+            int balance = 100;
+            int deposit = 50;
+            User expectedUser = new User(0)
+            {
+                Name = "DepositTestUser",
+                Username = "Deposit",
+                Balance = balance,
+                Email = "testuser@deposit.com",
+                Password = "test"
+            };
+            int id = DBUser.AddUser(expectedUser);
+            bool flag = DBUser.Deposit(deposit, id);
+            User actualUser = DBUser.GetUser(id);
+
+            if (flag) Assert.AreEqual(balance + deposit, actualUser.Balance);
+        }
+
+        [TestMethod]
+        public void PromotetoAdminTest()
+        {
+            User user = new User(0)
+            {
+                Name = "AdminTestUser",
+                Username = "Admin",
+                Balance = 5,
+                Email = "testadmin@admin.com",
+                Password = "test"
+            };
+
+            int promoteeId = DBUser.AddUser(user);
+            int promoterId = 1;//predefined admin in the db
+
+            bool flag = DBUser.PromotetoAdmin(promoterId, promoteeId);
+
+            Assert.IsTrue(DBUser.getIsAdmin(promoteeId));
+
+            //clean up
+            DBUser.DemoteAdmin(promoterId, promoteeId);
+        }
+
+        [TestMethod]
+        public void DemoteAdminTest()
+        {
+            User user = new User(0)
+            {
+                Name = "AdminTestUser",
+                Username = "Admin",
+                Balance = 5,
+                Email = "testadmin@admin.com",
+                Password = "test"
+            };
+
+            int demoteeId = DBUser.AddUser(user);
+            int demoterId = 1;//predefined admin in the db
+
+            //Promote admin 
+            DBUser.PromotetoAdmin(demoterId, demoteeId);
+
+            //Demote and assert
+            DBUser.DemoteAdmin(demoterId, demoteeId);
+
+            Assert.IsFalse(DBUser.getIsAdmin(demoteeId));
         }
     }
 }
