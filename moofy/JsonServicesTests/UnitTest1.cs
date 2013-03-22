7937582e2c3d30d9c430ac3ee1db0e75e6673502
Sigurt Bladt Dinesen <sigurt91@gmail.com>
2013-03-22 17:17:26 +0100

    modified:   JsonServicesTests.csproj modified:   UnitTest1.cs

--- moofy/JsonServicesTests/UnitTest1.cs
+++ moofy/JsonServicesTests/UnitTest1.cs
@@ -1,13 +1,14 @@
 ﻿using moofy.JsonServices;
-using System;
-using System.Net;
+using System; using System.Net;
 using System.Threading;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.ServiceModel.Description;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace moofy.JsonServices.Tests {
     
@@ -17,29 +18,29 @@ namespace moofy.JsonServices.Tests {
         static ServiceHost host = null;
 
         //This should probably be a test-utility class on its own
-        [AssemblyInitialize]
-        public static void InitiateServices(TestContext t) {
-            Uri baseAddr = new Uri("http://localhost:8732/moofytest");
-            string addr = "users";
+        //[AssemblyInitialize]
+        //public static void InitiateServices(TestContext t) {
+        //    Uri baseAddr = new Uri("http://localhost:8732/moofytest/");
+        //    string addr = "users";
+        //    host = new ServiceHost(typeof(MoofyServices), baseAddr);
+        //    host.AddServiceEndpoint(typeof(IUserService), new WebHttpBinding("system.web"), addr);
+        //    host.Open();
+        //}
 
-            host = new ServiceHost(typeof(MoofyServices), baseAddr);
-            host.AddServiceEndpoint(typeof(IUserService), new BasicHttpBinding(), addr);
-            host.Open();
-
-        }
-
-        [AssemblyCleanup]
-        public static void KillServices() {
-            Thread.Sleep(300);
-            host.Close();
-        }
+        //[AssemblyCleanup]
+        //public static void KillServices() {
+        //    Thread.Sleep(300);
+        //    host.Close();
+        //}
 
         [TestMethod]
         public void TestMethod1() {
             Uri u = new Uri("http://localhost:8732/moofytest/users/1");
             WebRequest r = WebRequest.Create(u);
             WebResponse res = r.GetResponse();
-            System.Console.WriteLine(res);
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string inputzlesz = reader.ReadToEnd();
+            res.Close();
         }
     }
 }
