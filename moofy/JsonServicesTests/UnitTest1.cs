aecf1e84752af7ca3561718419942a716e3918e1
Sigurt Bladt Dinesen <sigurt91@gmail.com>
2013-03-22 17:29:54 +0100

    modified:   BNDN-Report/jsontests.txt modified:   moofy/JsonServicesTests/UnitTest1.cs

--- moofy/JsonServicesTests/UnitTest1.cs
+++ moofy/JsonServicesTests/UnitTest1.cs
@@ -10,36 +10,54 @@ using System.ServiceModel.Description;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace moofy.JsonServices.Tests {
-    
+
+    private String users = new "http://localhost:8732/moofytest/users/";
+
     [TestClass]
     public class UnitTest1 {
 
         static ServiceHost host = null;
 
         //This should probably be a test-utility class on its own
-        [AssemblyInitialize]
-        public static void InitiateServices(TestContext t) {
-            Uri baseAddr = new Uri("http://localhost:8732/moofytest");
-            string addr = "users";
+        //[AssemblyInitialize]
+        //public static void InitiateServices(TestContext t) {
+        //    Uri baseAddr = new Uri("http://localhost:8732/moofytest");
+        //    string addr = "users";
 
-            host = new ServiceHost(typeof(MoofyServices), baseAddr);
-            host.AddServiceEndpoint(typeof(IUserService), new BasicHttpBinding(), addr);
-            host.Open();
+        //    host = new ServiceHost(typeof(MoofyServices), baseAddr);
+        //    host.AddServiceEndpoint(typeof(IUserService), new BasicHttpBinding(), addr);
+        //    host.Open();
 
-        }
+        //}
 
-        [AssemblyCleanup]
-        public static void KillServices() {
-            Thread.Sleep(300);
-            host.Close();
-        }
+        //[AssemblyCleanup]
+        //public static void KillServices() {
+        //    Thread.Sleep(300);
+        //    host.Close();
+        //}
 
+        //[TestMethod]
+        //public void TestMethod1() {
+        //    Uri u = new Uri("http://localhost:8732/moofytest/users/1");
+        //    WebRequest r = WebRequest.Create(u);
+        //    WebResponse res = r.GetResponse();
+        //    System.Console.WriteLine(res);
+        //}
         [TestMethod]
-        public void TestMethod1() {
-            Uri u = new Uri("http://localhost:8732/moofytest/users/1");
-            WebRequest r = WebRequest.Create(u);
+        public void ValidInUseIdOne(){
+            Uri u = new Uri(users+"1"),
+            WebRequest r = WebRequest.Create(users);
             WebResponse res = r.GetResponse();
-            System.Console.WriteLine(res);
+            StreamReader reader = new Streamreader(res.GetResponseStream());
+            String conts = reader.ReadToEnd();
+            bool isJsonUser = conts.StartsWith("{") &&
+                conts.EndsWith("}") &&
+                conts.Contains("balance") &&
+                conts.Contains("email") &&
+                conts.Contains("name") &&
+                conts.Contains("username");
+
+            Assert.IsTrue(isJsonUser, conts + " is not a valid user");
         }
     }
 }
