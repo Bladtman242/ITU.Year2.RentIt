3ee614e131fea4dbc710ee4c458380e37c0b2d88
Bladtman <sigurt91@gmail.com>
2013-03-23 21:05:11 +0100

    Still needs the non-GET services. modified:   moofy/JsonServicesTests/UnitTest1.cs

--- moofy/JsonServicesTests/UnitTest1.cs
+++ moofy/JsonServicesTests/UnitTest1.cs
@@ -17,6 +17,7 @@ namespace moofy.JsonServices.Tests {
     public class UnitTest1 {
         private String users = "http://localhost:8732/moofytest/users/";
         private String movies = "http://localhost:8732/moofytest/movies/";
+        private String songs = "http://localhost:8732/moofytest/songs/";
 
         //static ServiceHost host = null;
 
@@ -310,8 +311,313 @@ namespace moofy.JsonServices.Tests {
         }
 
         /*
-         * moofytest/movies
+         * moofytest/users/songs/
          */
+
+        [TestMethod]
+        public void UsersSongsValidInUseIdOne() {
+            string usongs = users + "1/songs";
+            var res = GetMessage(usongs);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isJsonSongList = IsSongList(conts);
+            Assert.AreEqual(HttpStatusCode.OK, status);
+            Assert.IsTrue(isJsonSongList, conts + " is not a valid song list");
+        }
+
+        [TestMethod]
+        public void UsersSongsValidInUseId7k() {
+            string usongs = users + "70000/songs";
+            var res = GetMessage(usongs);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isJsonSongList = IsSongList(conts);
+            Assert.AreEqual(HttpStatusCode.OK, status);
+            Assert.IsTrue(isJsonSongList, conts + " is not a valid song list");
+        }
+
+        [TestMethod]
+        public void UsersSongsValidInUseNoMovs() {
+            string usongs = users + "42/songs";
+            var res = GetMessage(usongs);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            Assert.AreEqual(HttpStatusCode.OK, status);
+            Assert.AreEqual(conts, "[]"); //this it?
+        }
+
+        [TestMethod]
+        public void UsersSongsValidVacantIdTwo() {
+            string usongs = users + "2/songs";
+            var res = GetMessage(usongs);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void UsersSongsInValidIdNegOne() {
+            string usongs = users + "-1/songs";
+            var res = GetMessage(usongs);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void UsersSongsInValidIdNeg7k() {
+            string usongs = users + "-70000/songs";
+            var res = GetMessage(usongs);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void UsersSongsInValidTypeTwenty() {
+            string usongs = users + "twenty/songs";
+            var res = GetMessage(usongs);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void UsersSongsInValidTypeKatmandu() {
+            string usongs = users + "katmandu/songs";
+            var res = GetMessage(usongs);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        /*
+         * moofytest/songs
+         */
+
+        [TestMethod]
+        public void SongsValidInUseIdOne() {
+            string s = songs + "1";
+            var res = GetMessage(s);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isJsonSong = IsSongList(conts);
+            Assert.IsTrue(isJsonSong, conts + " is not a valid song");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        [TestMethod]
+        public void SongValidInUseIdLarge() {
+            string s = songs + "70000";
+            var res = GetMessage(s);
+            string conts = res.contents;
+
+            bool isJsonSong = IsSong(conts);
+            Assert.IsTrue(isJsonSong, conts + " is not a valid song");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        [TestMethod]
+        public void SongsValidVacantId2() {
+            string s = songs + "2";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsInValidIdZero() {
+            string s = songs + "0";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsInValidIdNegOne() {
+            string s = songs + "-1";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsInValidIdNeg7k() {
+            string s = songs + "-70000";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsInValidTypeTwenty() {
+            string s = songs + "twenty";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsInValidTypeKatmandu() {
+            string s = songs + "Katmandu";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        /*
+        * moofytest/songs/download
+        */
+
+        [TestMethod]
+        public void SongsDownloadValidInUseIdOne() {
+            string s = songs + "1/download";
+            var res = GetMessage(s);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isSongLnk = IsSongDownLink(conts);
+            Assert.IsTrue(isSongLnk, conts + " is not a valid link");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        [TestMethod]
+        public void SongsDownloadValidInUseIdLarge() {
+            string s = songs + "70000/download";
+            var res = GetMessage(s);
+            string conts = res.contents;
+
+            bool isSongLnk = IsSongDownLink(conts);
+            Assert.IsTrue(isSongLnk, conts + " is not a valid link");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        [TestMethod]
+        public void SongsDownloadValidVacantId2() {
+            string s = songs + "2/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsDownloadInValidIdZero() {
+            string s = songs + "0/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsDownloadInValidIdNegOne() {
+            string s = songs + "-1/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsDownloadInValidIdNeg7k() {
+            string s = songs + "-70000/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsDownloadInValidTypeTwenty() {
+            string s = songs + "twenty/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        [TestMethod]
+        public void SongsDownloadInValidTypeKatmandu() {
+            string s = songs + "Katmandu/download";
+            var res = GetMessage(s);
+
+            HttpStatusCode status = res.status;
+            Assert.AreEqual(HttpStatusCode.NotFound, status);
+        }
+
+        /*
+         * moofytest/songs/filter
+         */
+
+        [TestMethod]
+        public void SongsFilter() {
+            string s = songs + "filter/";
+            var res = GetMessage(s);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isSongList = IsSongList(conts);
+            //TODO doesn't test whether all movies are represented, not sure if it should either
+            Assert.IsTrue(isSongList, conts + " is not a valid song list");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        /*
+         * moofytest/songs/filter/{filter}
+         */
+
+        ///TODO  remove uselessness
+        [TestMethod]
+        public void SongsFilterFilter() {
+            string s = movies + "filter/lars";
+            var res = GetMessage(s);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isMovList = IsMovList(conts);
+            Assert.IsTrue(isMovList, conts + " is not a valid song list");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        /*
+         * moofytest/songs/filter/{filter}?sortBy={sortBy}
+         */
+
+        ///TODO  remove uselessness
+        [TestMethod]
+        public void SongsFilterSort() {
+            string s = songs + "filter/lars?sortBy=title";
+            var res = GetMessage(s);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isMovList = IsMovList(conts);
+            Assert.IsTrue(isMovList, conts + " is not a valid song list");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        /*
+        * moofytest/songs
+        */
 
         [TestMethod]
         public void MoviesValidInUseIdOne() {
@@ -495,6 +801,37 @@ namespace moofy.JsonServices.Tests {
          * moofytest/movies/filter/{filter}
          */
 
+        ///TODO  remove uselessness
+        [TestMethod]
+        public void MoviesFilterFilter() {
+            string m = movies + "filter/lars";
+            var res = GetMessage(m);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isMovList = IsMovList(conts);
+            Assert.IsTrue(isMovList, conts + " is not a valid movie list");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
+
+        /*
+         * moofytest/movies/filter/{filter}?sortBy={sortBy}
+         */
+
+        ///TODO  remove uselessness
+        [TestMethod]
+        public void MoviesFilterSort() {
+            string m = movies + "filter/lars?sortBy=title";
+            var res = GetMessage(m);
+
+            string conts = res.contents;
+            HttpStatusCode status = res.status;
+
+            bool isMovList = IsMovList(conts);
+            Assert.IsTrue(isMovList, conts + " is not a valid movie list");
+            Assert.AreEqual(HttpStatusCode.OK, res.status); //assert 200
+        }
         /*
          * HELPERS
          */
@@ -519,6 +856,41 @@ namespace moofy.JsonServices.Tests {
             return new Mess { status = res.StatusCode, contents = conts };
         }
 
+        //Helper for checking json data for songs
+        private bool IsSong(string s) {
+            return s.StartsWith("{") &&
+                s.EndsWith("}") &&
+                s.Contains("album") &&
+                s.Contains("artist") &&
+                s.Contains("genres") &&
+                s.Contains("purchasePrice") &&
+                s.Contains("release") &&
+                s.Contains("rentalPrice") &&
+                s.Contains("title");
+        }
+
+        //Helper for checking json data for songs
+        private bool IsSongList(string s) {
+            return s.StartsWith("[{") &&
+                s.EndsWith("}]") &&
+                s.Contains("album") &&
+                s.Contains("artist") &&
+                s.Contains("genres") &&
+                s.Contains("purchasePrice") &&
+                s.Contains("release") &&
+                s.Contains("rentalPrice") &&
+                s.Contains("title");
+        }
+
+        //Helper for checking json data for song downloadlink
+        private bool IsSongDownLink(string sd) {
+            return sd.StartsWith("{") &&
+                sd.EndsWith("}") &&
+                sd.Contains("downloadlink") &&
+                sd.Contains("success") &&
+                (sd.Contains("false") || sd.Contains("true"));
+        }
+        
         //Helper for checking json data for users
         private bool IsUser(string u){
             return u.StartsWith("{") &&
