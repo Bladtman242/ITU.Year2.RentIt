e3a2532f06543b58ec32f3fec58f3946ecb4cc9b
Gansted <kbri@itu.dk>
2013-03-25 14:40:26 +0100

    reverted to visual studio 2010 project

---
+++ moofy/BackendTests/DBMovieTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace moofy.Backend.Tests
+{
+    /// <summary>
+    /// Summary description for DBMovieTest
+    /// </summary>
+    [TestClass]
+    public class DBMovieTest
+    {
+        static DBAccess db;
+
+        public DBMovieTest() {
+            db = new DBAccess();
+            db.Open();
+        }
+
+        [ClassCleanup()]
+        public static void CleanUp() {
+            db.Close();
+        }
+
+        private TestContext testContextInstance;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
