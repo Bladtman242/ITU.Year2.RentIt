0eb2880642d1a43b55ebfbc7d81a1f86b3d047c6
Niclas Tollstorff <niclastollstorff@gmail.com>
2013-04-02 21:59:21 +0200

    Added cover uri property for gansted

--- moofy/Backend/File.cs
+++ moofy/Backend/File.cs
@@ -32,35 +32,6 @@ namespace moofy.Backend {
         public string Description { get; set; }
 
         /// <summary>
-        /// The Average rating of the File
-        /// </summary>
-        public float AverageRating {
-            get {
-                if (averageRating == null) {
-                    db.Open();
-                    averageRating = db.GetRating(this.Id);
-                    db.Close();
-                }
-                return (float)averageRating;
-            }
-        }
-
-        /// <summary>
-        /// The number of ratings of the File
-        /// </summary>
-        public int NumberOfRatings {
-            get {
-                if (numberOfRatings == null) {
-                    db.Open();
-                    numberOfRatings = db.GetNumberOfRatings(this.Id);
-                    db.Close();
-                }
-                return (int)numberOfRatings;
-            }
-        }
-
-
-        /// <summary>
         /// The Genre of the File
         /// </summary>
         public IList<string> Genres {
@@ -88,6 +59,11 @@ namespace moofy.Backend {
         public string Uri { get; set; }
 
         /// <summary>
+        /// The location of the files cover picture as a URI
+        /// </summary>
+        public string CoverUri { get; set; }
+
+        /// <summary>
         /// The cost of the File, when bought
         /// </summary>
         public int BuyPrice { get; set; }
@@ -96,6 +72,34 @@ namespace moofy.Backend {
         /// The cost of the File when rented
         /// </summary>
         public int RentPrice { get; set; }
+
+        /// <summary>
+        /// The Average rating of the File
+        /// </summary>
+        public float AverageRating {
+            get {
+                if (averageRating == null) {
+                    db.Open();
+                    averageRating = db.GetRating(this.Id);
+                    db.Close();
+                }
+                return (float)averageRating;
+            }
+        }
+
+        /// <summary>
+        /// The number of ratings of the File
+        /// </summary>
+        public int NumberOfRatings {
+            get {
+                if (numberOfRatings == null) {
+                    db.Open();
+                    numberOfRatings = db.GetNumberOfRatings(this.Id);
+                    db.Close();
+                }
+                return (int)numberOfRatings;
+            }
+        }
 
     }
 }
