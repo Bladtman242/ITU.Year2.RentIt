07ce8d6ee36b2cf3d753f854cf471374ef3b8da1
Kristian Gansted <kgansted@gmail.com>
2013-04-23 14:52:56 +0200

    Fixed clear db method to fit new design

--- moofy/Backend/DBAccess.cs
+++ moofy/Backend/DBAccess.cs
@@ -26,6 +26,8 @@ namespace moofy.Backend {
         public void ClearDatabase()
         {
             SqlCommand command = new SqlCommand("IF OBJECT_ID('StagedFile') IS NOT NULL DROP TABLE StagedFile " +
+                                                "IF OBJECT_ID('MovieDirector') IS NOT NULL DROP TABLE MovieDirector "+
+                                                "IF OBJECT_ID('SongArtist') IS NOT NULL DROP TABLE SongArtist "+
                                                 "IF OBJECT_ID('Movie') IS NOT NULL DROP TABLE Movie " +
                                                 "IF OBJECT_ID('Song') IS NOT NULL DROP TABLE Song " +
                                                 "IF OBJECT_ID('GenreFile') IS NOT NULL DROP TABLE GenreFile " +
@@ -35,10 +37,16 @@ namespace moofy.Backend {
                                                 "IF OBJECT_ID('Genre') IS NOT NULL DROP TABLE Genre " +
                                                 "IF OBJECT_ID('Admin') IS NOT NULL DROP TABLE Admin " +
                                                 "IF OBJECT_ID('Users') IS NOT NULL DROP TABLE Users " +
+                                                "IF OBJECT_ID('Director') IS NOT NULL DROP TABLE Director "+
+                                                "IF OBJECT_ID('Artist') IS NOT NULL DROP TABLE Artist "+
                                                 "CREATE TABLE StagedFile (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, path VARCHAR(200) NOT NULL) "+
                                                 "CREATE TABLE Files (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,title VARCHAR(200) NOT NULL,rentPrice INT NOT NULL, buyPrice INT NOT NULL, URI VARCHAR(200) NOT NULL, year INT NOT NULL, description VARCHAR(1000), coverURI VARCHAR(200) NOT NULL, viewCount INT NOT NULL) "+
-                                                "CREATE TABLE Movie (id INT PRIMARY KEY NOT NULL, director VARCHAR(150), FOREIGN KEY (id) REFERENCES Files(id)) "+
-                                                "CREATE TABLE Song (id INT PRIMARY KEY NOT NULL, artist VARCHAR(150), album VARCHAR(150), FOREIGN KEY (id) REFERENCES Files(id)) "+
+                                                "CREATE TABLE Movie (id INT PRIMARY KEY NOT NULL, FOREIGN KEY (id) REFERENCES Files(id)) "+
+                                                "CREATE TABLE Director (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, name VARCHAR(150)) "+
+                                                "CREATE TABLE MovieDirector (moid INT NOT NULL, did INT NOT NULL, FOREIGN KEY (moid) REFERENCES Movie(id), FOREIGN KEY (did) REFERENCES Director(id)) "+
+                                                "CREATE TABLE Song (id INT PRIMARY KEY NOT NULL, album VARCHAR(150), FOREIGN KEY (id) REFERENCES Files(id)) "+
+                                                "CREATE TABLE Artist (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, name VARCHAR(150)) "+
+                                                "CREATE TABLE SongArtist (sid INT NOT NULL, aid INT NOT NULL, FOREIGN KEY (sid) REFERENCES Song(id), FOREIGN KEY (aid) REFERENCES Artist(id)) "+
                                                 "CREATE TABLE Genre (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, name VARCHAR(150)) "+
                                                 "CREATE TABLE GenreFile (gid INT NOT NULL, fid INT NOT NULL, PRIMARY KEY (gid, fid), FOREIGN KEY (gid) REFERENCES Genre(id), FOREIGN KEY (fid) REFERENCES Files(id)) "+
                                                 "CREATE TABLE Users (id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, userName VARCHAR(150) NOT NULL, password VARCHAR(150) NOT NULL, name VARCHAR(150), email VARCHAR(150) UNIQUE, balance INT NOT NULL) "+
@@ -53,10 +61,14 @@ namespace moofy.Backend {
                                                 "('Life of a Small Son Title Track (Small Son)', 2, 5, 'rentit.itu.dk/RentIt25/downloads/flute.mp3', 2013, 'Awesome flutez', 'http://4.bp.blogspot.com/-1tmYxvB58CY/UFjMqzTdQKI/AAAAAAAAH_M/g7kWAu4Y4kc/s1600/600px-No_image_available.svg.png',0), "+
                                                 "('A', 1, 2, '', 2912, 'Hej', 'URILOL', 4000), " +
                                                 "('Sang', 55, 345, '', 93, 'dsffd', '.', 1) " +
-                                                "INSERT INTO Movie VALUES(1,'Niclas Benjamin Tollstorff'), "+
-                                                "(3, 'Søren') " +
-                                                "INSERT INTO Song VALUES(2, 'Vanilla Ice', 'Life of a Small Son Soundtrack'), "+
-                                                "(4, 'Xzibit', 'Xzibits album') " +
+                                                "INSERT INTO Movie VALUES(1), (3) " +
+                                                "INSERT INTO Director VALUES('Niclas Benjamin Tollstorff'), "+
+                                                "('Søren') "+
+                                                "INSERT INTO MovieDirector VALUES(1,1),(3,1) " +
+                                                "INSERT INTO Song VALUES(2, 'Life of a Small Son Soundtrack'), "+
+                                                "(4, 'Xzibits album') " +
+                                                "INSERT INTO Artist VALUES ('Vanilla Ice'), ('Xzibit') "+
+                                                "INSERT INTO SongArtist VALUES (2,1),(4,2) "+
                                                 "INSERT INTO Genre VALUES('Dance') "+
                                                 "INSERT INTO GenreFile VALUES(1,2)",
                                                 connection);
