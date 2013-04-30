7c4fa8e7f48b86714bf9617b2a24c12c3d8668dc
Niclas Tollstorff <niclastollstorff@gmail.com>
2013-04-30 22:25:21 +0200

    Added modal functionality to output library

--- client/output.js
+++ client/output.js
@@ -15,6 +15,31 @@ var output = new function(){
             <strong/> <span/>\
         </div>");
 
+    var templateActionModal = $(
+        "<div class='modal hide'>\
+            <div class='modal-header'>\
+                <button type='button' class='close' data-dismiss='modal'>&times</button>\
+                <h3/>\
+            </div>\
+            <div class='modal-body'/>\
+            <div class='modal-footer'>\
+                <button class='btn' data-dismiss='modal'>Cancel</button>\
+                <button class='btn btn-primary'/>\
+            </div>\
+        </div>");
+
+    var templateModal = $(
+        "<div class='modal hide'>\
+            <div class='modal-header'>\
+                <button type='button' class='close' data-dismiss='modal'>&times</button>\
+                <h3/>\
+            </div>\
+            <div class='modal-body'/>\
+            <div class='modal-footer'>\
+                <button class='btn' data-dismiss='modal'>Cancel</button>\
+            </div>\
+        </div>");
+
     //Public functions
     this.error = function(bold, nonBold) {
         templateErrorAlert.children().eq(1).html(bold);
@@ -30,8 +55,24 @@ var output = new function(){
         framework.container.prepend(templateSuccessAlert);
     };
 
-    this.modal = function(header, body, actionCallback) {
-        $('.modal').remove
-
+    this.modal = function(header, body, actionButtonText, actionFunction) {
+        $('.modal').remove();
+        if (actionButtonText && actionFunction) { //use the action modal
+            templateActionModal.children().eq(0).children().eq(1).text(header);
+            templateActionModal.children().eq(1).html(body);
+            var button = templateActionModal.children().eq(2).children().eq(1);
+            button.text(actionButtonText);
+            button.click(function() {
+                templateActionModal.modal('hide'); //Hide the modal window
+                actionFunction();
+            });
+            framework.container.prepend(templateActionModal);
+            templateActionModal.modal(null);
+        }else { //Use the one with the cancel button only
+            templateModal.children().eq(0).children().eq(1).text(header);
+            templateModal.children().eq(1).html(body);
+            framework.container.prepend(templateModal);
+            templateModal.modal(null);
+        }
     };
 };
